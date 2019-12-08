3d6bdaa289ad9414df5433d7130f076800ba8066
pedrothome1 <pedrothome2@outlook.com>
2019-12-08 12:07:48 -0300

    setting up nodemailer and formatting welcome mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  default: {
+    from: process.env.MAIL_FROM,
+  },
+};
